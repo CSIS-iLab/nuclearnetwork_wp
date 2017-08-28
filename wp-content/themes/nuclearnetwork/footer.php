6c0c8e9bb014fa5705556f92b787e6a9f8519043
Jacque Schrag <jnschrag@gmail.com>
2017-08-28 17:05:28 -0400

    Render footer HTML and begin basic styling

--- wp-content/themes/nuclearnetwork/footer.php
+++ wp-content/themes/nuclearnetwork/footer.php
@@ -9,24 +9,95 @@
  * @package Nuclear_Network
  */
 
+$description = get_option( 'nuclearnetwork_description' );
+$disclaimer = get_option( 'nuclearnetwork_disclaimer' );
+$address = get_option( 'nuclearnetwork_address' );
+$email = get_option( 'nuclearnetwork_email' );
+$phone = get_option( 'nuclearnetwork_phone' );
+$facebook = get_option( 'nuclearnetwork_facebook' );
+$twitter = get_option( 'nuclearnetwork_twitter' );
+$linkedin = get_option( 'nuclearnetwork_linkedin' );
+$newsletter_monthly_desc = get_option( 'nuclearnetwork_newsletter_monthly_desc' );
+$newsletter_monthly_url = get_option( 'nuclearnetwork_newsletter_monthly_url' );
+$newsletter_daily_desc = get_option( 'nuclearnetwork_newsletter_daily_desc' );
+$newsletter_daily_url = get_option( 'nuclearnetwork_newsletter_daily_url' );
+
 ?>
 
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nuclearnetwork' ) ); ?>">
-								<?php
-								/* translators: %s: CMS name, i.e. WordPress. */
-								printf( esc_html__( 'Proudly powered by %s', 'nuclearnetwork' ), 'WordPress' );
-			?>
-			</a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'nuclearnetwork' ), 'nuclearnetwork', '<a href="https://github.com/CSIS-iLab/">CSIS iDeas Lab</a>' );
-			?>
+		<div class="site-info content-wrapper row">
+			<div class="footer-info col-xs-12 col-md-6">
+				CSIS Logo<br />
+				<?php
+				if ( $description ) {
+					echo '<p>' . esc_html( $description ) . '</p>';
+				}
+				?>
+				<?php
+				if ( $disclaimer ) {
+					echo '<p class="footer-disclaimer">' . esc_html( $disclaimer ) . '</p>';
+				}
+				?>
+			</div>
+			<div class="footer-contact col-xs-12 col-md-2">
+				<h5 class="section-title"><?php esc_html_e( 'Contact Us', 'nucleartnetwork' ); ?></h5>
+				<?php
+				if ( $address ) {
+					echo '<h6 class="section-subtitle">' . esc_html( 'Address', 'nuclearnetwork' ) . '</h6>';
+					echo '<address>' . wp_kses_post( $address ) . '</address>';
+				}
+				?>
+				<?php
+				if ( $email ) {
+					echo '<h6 class="section-subtitle">' . esc_html( 'Email', 'nuclearnetwork' ) . '</h6>';
+					echo '<a href="mailto:' . esc_attr( $email ) . '?subject=' . esc_attr( get_bloginfo( 'name' ) ) . '">' . esc_html( $email ) . '</a>';
+				}
+				?>
+				<?php
+				if ( $phone ) {
+					echo '<h6 class="section-subtitle">' . esc_html( 'Phone', 'nuclearnetwork' ) . '</h6>';
+					echo '<p>' . esc_html( $phone ) . '</p>';
+				}
+				?>
+				<ul class="footer-social">
+					<?php
+					if ( $facebook ) {
+						echo '<li><a href="' . esc_url( $facebook ) . '">Facebook</a></li>';
+					}
+					if ( $twitter ) {
+						echo '<li><a href="https://twitter.com/' . esc_attr( $twitter ) . '">Twitter</a></li>';
+					}
+					if ( $linkedin ) {
+						echo '<li><a href="' . esc_url( $linkedin ) . '">LinkedIn</a></li>';
+					}
+					?>
+				</ul>
+			</div>
+			<div class="footer-newsletters col-xs-12 col-md-2">
+				<h5 class="section-title"><?php esc_html_e( 'Stay up to date', 'nuclearnetwork' ); ?></h5>
+				<?php
+				if ( $newsletter_monthly_url && $newsletter_monthly_desc ) {
+					echo '<h6 class="section-subtitle">' . esc_html( 'Monthly PONI Newsletter', 'nuclearnetwork' ) . '</h6>';
+					echo '<p>' . esc_html( $newsletter_monthly_desc ) . '</p>';
+					echo '<a href="' . esc_attr( $newsletter_monthly_url ) . '" class="btn btn-blue">' . esc_html( 'Sign Up', 'nuclearnetwork' ) . '</a>';
+				}
+				?>
+				<?php
+				if ( $newsletter_daily_url && $newsletter_daily_desc ) {
+					echo '<h6 class="section-subtitle">' . esc_html( 'Daily PONI Newsletter', 'nuclearnetwork' ) . '</h6>';
+					echo '<p>' . esc_html( $newsletter_daily_desc ) . '</p>';
+					echo '<a href="' . esc_attr( $newsletter_daily_url ) . '" class="btn btn-blue">' . esc_html( 'Sign Up', 'nuclearnetwork' ) . '</a>';
+				}
+				?>
+			</div>
 		</div><!-- .site-info -->
+		<div class="footer-copyright">
+			<div class="content-wrapper">
+				<p>&copy; <?php echo esc_html( date( 'Y' ) ); ?> by the Center for Strategic and International Studies. All rights reserved.</p>
+			</div>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
