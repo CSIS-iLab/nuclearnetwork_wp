e692ba292fa9bc32d0763c1137c744fbf2bfb67b
Jacque Schrag <jnschrag@gmail.com>
2017-09-12 18:02:37 -0400

    Make sidebar sticky

--- wp-content/themes/nuclearnetwork/js/posts.js
+++ wp-content/themes/nuclearnetwork/js/posts.js
@@ -23,4 +23,56 @@
     }, 1000);
   });
 
+	// Sticky Menu
+	var windw = this;
+
+	$.fn.followTo = function (from, bumper) {
+	    var $this = this,
+	        $window = $(windw),
+	        $from = $(from),
+	        $bumper = $(bumper),
+	        $startPos = $from.offset().top + $from.outerHeight(true),
+	        $startPosMinimalHeader = $from.offset().top + $from.outerHeight(true) - 25,
+	        bumperPos = $bumper.offset().top,
+	        thisHeight = $this.outerHeight(true),
+	        $headerHeight = $(".site-header").height(),
+	        setPosition = function(){
+
+	            if ($window.scrollTop() == 0 ) { 
+	                $this.css({
+	                    position: 'absolute',
+	                    top: $startPos
+	                });
+	            } else if ($window.scrollTop() < $startPos - 100 ) {
+	            	$this.css({
+	                    position: 'absolute',
+	                    top: $startPosMinimalHeader
+	                });
+	            } else if ($window.scrollTop() > (bumperPos - thisHeight - 175)) {
+	                $this.css({
+	                    position: 'absolute',
+	                    top: (bumperPos - thisHeight - 75)
+	                });
+	            } else {
+	                $this.css({
+	                    position: 'fixed',
+	                    top: $headerHeight
+	                });
+	            }
+	        };
+	    $window.resize(function()
+	    {
+	        bumperPos = $bumper.offset().top;
+	        thisHeight = $this.outerHeight();
+	        setPosition();
+	    });
+	    $window.scroll(setPosition);
+	    setPosition();
+	};
+
+	var sidebarPos = $('.post-sidebar').css('position');
+	if(sidebarPos == 'fixed') {
+		$('.post-sidebar').followTo('.entry-header', '.entry-footer');
+	}
+
 } )( jQuery );
