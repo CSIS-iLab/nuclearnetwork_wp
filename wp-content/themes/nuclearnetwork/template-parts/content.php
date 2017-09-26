c1c4a94b5ce07909bcfa728a4ee192c26e858d25
Jacque Schrag <jnschrag@gmail.com>
2017-09-26 13:45:20 -0400

    Featured post format on archive pages

--- wp-content/themes/nuclearnetwork/template-parts/content.php
+++ wp-content/themes/nuclearnetwork/template-parts/content.php
@@ -7,6 +7,43 @@
  * @package Nuclear_Network
  */
 
+$is_featured = get_post_meta( $id, '_post_is_featured', true );
+
+if ( 1 == $is_featured ) {
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'article-card featured' ); ?>>
+		<header class="entry-header">
+			<?php
+			nuclearnetwork_post_format( $post->ID );
+			if ( has_post_thumbnail() ) {
+				echo '<div class="post-thumbnail"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">';
+				the_post_thumbnail( 'medium_large' );
+				echo '</a></div>';
+			}
+			echo '<div class="post-header"><div class="title-container">';
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			echo '</div></div>';
+
+			if ( 'post' === get_post_type() ) :
+			?>
+			<div class="entry-meta">
+				<?php
+				nuclearnetwork_authors_list();
+				nuclearnetwork_posted_on();
+				nuclearnetwork_entry_categories();
+				?>
+			</div><!-- .entry-meta -->
+			<?php endif; ?>
+		</header><!-- .entry-header -->
+
+		<div class="entry-content">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-content -->
+</article><!-- #post-<?php the_ID(); ?> -->
+
+<?php
+} else {
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'article-card row' ); ?>>
@@ -14,7 +51,7 @@
 	if ( has_post_thumbnail() ) {
 		echo '<div class="col-xs-12 col-md-4 post-thumbnail"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">';
 		the_post_thumbnail( 'medium_large' );
-		echo "</a></div>";
+		echo '</a></div>';
 	}
 	?>
 	<div class="col-xs-12 col-md">
@@ -27,7 +64,8 @@
 				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 			endif;
 
-			if ( 'post' === get_post_type() ) : ?>
+			if ( 'post' === get_post_type() ) :
+			?>
 			<div class="entry-meta">
 				<?php
 				nuclearnetwork_authors_list();
@@ -35,8 +73,7 @@
 				nuclearnetwork_entry_categories();
 				?>
 			</div><!-- .entry-meta -->
-			<?php
-			endif; ?>
+			<?php endif; ?>
 		</header><!-- .entry-header -->
 
 		<div class="entry-content">
@@ -44,3 +81,4 @@
 		</div><!-- .entry-content -->
 	</div>
 </article><!-- #post-<?php the_ID(); ?> -->
+<?php } ?>
