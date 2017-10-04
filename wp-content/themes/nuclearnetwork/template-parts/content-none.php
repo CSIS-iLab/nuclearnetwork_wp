9fb261c7555a6ad8a5891ef337dd141140d9c5e2
Jacque Schrag <jnschrag@gmail.com>
2017-10-04 12:01:16 -0400

    Formatted no results found entry.

--- wp-content/themes/nuclearnetwork/template-parts/content-none.php
+++ wp-content/themes/nuclearnetwork/template-parts/content-none.php
@@ -10,41 +10,12 @@
 ?>
 
 <section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'nuclearnetwork' ); ?></h1>
+	<header class="entry-header">
+		<h2 class="entry-title"><?php esc_html_e( 'No Results Found', 'nuclearnetwork' ); ?></h1>
 	</header><!-- .page-header -->
 
-	<div class="page-content">
-		<?php
-		if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-
-			<p><?php
-				printf(
-					wp_kses(
-						/* translators: 1: link to WP admin new post page. */
-						__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'nuclearnetwork' ),
-						array(
-							'a' => array(
-								'href' => array(),
-							),
-						)
-					),
-					esc_url( admin_url( 'post-new.php' ) )
-				);
-			?></p>
-
-		<?php elseif ( is_search() ) : ?>
-
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'nuclearnetwork' ); ?></p>
-			<?php
-				get_search_form();
-
-		else : ?>
-
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'nuclearnetwork' ); ?></p>
-			<?php
-				get_search_form();
-
-		endif; ?>
+	<div class="entry-content">
+		<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords or filtering options.', 'nuclearnetwork' ); ?></p>
+		<?php get_search_form(); ?>
 	</div><!-- .page-content -->
 </section><!-- .no-results -->
