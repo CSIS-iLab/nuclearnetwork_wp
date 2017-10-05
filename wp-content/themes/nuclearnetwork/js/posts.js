81776b7226d84820fc577fafb28a24c585fc9e49
Jacque Schrag <jnschrag@gmail.com>
2017-10-05 11:40:36 -0400

    Add smooth scrolling for anchor links

--- wp-content/themes/nuclearnetwork/js/posts.js
+++ wp-content/themes/nuclearnetwork/js/posts.js
@@ -95,4 +95,18 @@
 	  stickySidebar();
 	});
 
+	// Smooth Scrolling
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top - 100
+				}, 1500);
+				return false;
+			}
+		}
+	});
+
 } )( jQuery );
