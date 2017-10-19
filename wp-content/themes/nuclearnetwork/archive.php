b8e4483b5efd118fffb1a4e155f442be126d9154
Jacque Schrag <jnschrag@gmail.com>
2017-10-19 11:03:12 -0400

    Add custom sort order to resources posts

--- wp-content/themes/nuclearnetwork/archive.php
+++ wp-content/themes/nuclearnetwork/archive.php
@@ -46,17 +46,42 @@ $featured_img = ' style="background-image:url(\'' . esc_attr( $img ) . '\');"';
 
 					nuclearnetwork_post_num();
 
-					/* Start the Loop */
-					while ( have_posts() ) : the_post();
+					if ( is_post_type_archive( 'resources' ) ) {
+						$types = get_terms( 'resource_types' );
+						foreach ( $types as $type ) {
+							$resource_posts = new WP_Query( array(
+								'post_type' => 'resources',
+								'tax_query' => array(
+									array(
+										'taxonomy' => 'resource_types',
+										'field' => 'slug',
+										'terms' => array( $type->slug ),
+										'operator' => 'IN',
+									),
+								),
+								'orderby' => 'title',
+								'order' => 'ASC',
+							) );
 
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'template-parts/content', get_post_type() );
+							while ( $resource_posts->have_posts() ) : $resource_posts->the_post();
+								get_template_part( 'template-parts/content', 'resources' );
+							endwhile;
+							unset( $resource_posts );
+							wp_reset_postdata();
+						}
+					} else {
+						/* Start the Loop */
+						while ( have_posts() ) : the_post();
 
-					endwhile;
+							/*
+							 * Include the Post-Format-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content', get_post_type() );
+
+						endwhile;
+					}
 
 					the_posts_pagination();
 
