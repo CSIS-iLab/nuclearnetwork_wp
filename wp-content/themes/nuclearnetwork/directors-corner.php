50ab86646c32a30a10f3087af95cbaa4963b6f42
Serven Maraghi <maraghiserven@gmail.com>
2020-02-02 19:38:27 -0500

    Add new template for directors corner

---
+++ wp-content/themes/nuclearnetwork/directors-corner.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The template for the Director's Corner
+ *
+ * Template Name: Director's Corner
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Nuclear_Network
+ */
+
+?>
