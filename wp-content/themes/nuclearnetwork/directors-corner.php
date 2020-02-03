9fc980e97fd30e99da7d126e84b38b641fe41c2d
Serven Maraghi <maraghiserven@gmail.com>
2020-02-02 19:43:48 -0500

    Add header to template

--- wp-content/themes/nuclearnetwork/directors-corner.php
+++ wp-content/themes/nuclearnetwork/directors-corner.php
@@ -9,4 +9,16 @@
  * @package Nuclear_Network
  */
 
+get_header();
+
+if ( is_category() || is_tag() ) {
+	$img = get_option( 'nuclearnetwork_category_and_tag_archive_image' );
+} elseif ( is_author() ) {
+	$img = get_option( 'nuclearnetwork_authors_archive_image' );
+} else {
+	$img = get_archive_thumbnail_src( 'full' );
+}
+
+$featured_img = ' style="background-image:url(\'' . esc_attr( $img ) . '\');"';
+
 ?>
