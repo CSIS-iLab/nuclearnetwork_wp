a76f5639556d472e6fcf585fbabad87097a836e0
Serven Maraghi <maraghiserven@gmail.com>
2020-02-03 17:12:03 -0500

    Modify directors corner template structure

--- wp-content/themes/nuclearnetwork/directors-corner.php
+++ wp-content/themes/nuclearnetwork/directors-corner.php
@@ -11,10 +11,6 @@
 
 get_header();
 
-// $wp_query = new WP_Query(array(
-//   'author_name' => 'rebecca-hersman'
-// ));
-
 if ( is_category() || is_tag() ) {
 	$img = get_option( 'nuclearnetwork_category_and_tag_archive_image' );
 } elseif ( is_author() ) {
@@ -28,71 +24,36 @@ $featured_img = ' style="background-image:url(\'' . esc_attr( $img ) . '\');"';
 ?>
 
 <div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main" class="site-main content-wrapper">
 			<header class="page-header"<?php echo $featured_img; ?>>
-      <div class="header-content">
-					<?php
-					echo '<div class="archive-category">';
-					the_archive_top_content();
-					echo '</div>';
-					nuclearnetwork_directors_list();
-   
-					// Don't show description on author pages because the bios are too long.
-					if ( ! is_author() ) {
-						echo '<div class="archive-description">';
-						the_archive_bottom_content();
-						echo '</div>';
-					}
-					?>
-				</div>
-			
+				<div class="header-content"></div>
 			</header><!-- .page-header -->
 
 			<div class="content-wrapper row archive-container">
+
 				<div class="col-xs-12 col-md-9 archive-content">
-				<?php
+				
+				
+				
+<?php
 
-        // Show description in post section if author page.
-				if ( is_author() ) {
-          the_archive_description( '<div class="archive-description">', '</div>' );
-        }
-        
+	while ( have_posts() ) : the_post();
 
-				if ( have_posts() ) :
+		get_template_part( 'template-parts/content', 'page' );
+		
 
-					nuclearnetwork_post_num();
+	endwhile; // End of the loop.
 
-					/* Start the Loop */
-					while ( have_posts() ) : the_post();
+	the_posts_pagination();
 
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'template-parts/content-alumni', get_the_excerpt() );
-
-					endwhile;
-
-					the_posts_pagination();
-
-				else :
-
-					get_template_part( 'template-parts/content', 'none' );
-
-				endif;
-
-				nuclearnetwork_event_future_past_link();
-
-				?>
-				</div>
-				<div class="col-xs-12 col-md-3 archive-sidebar">
-					<?php get_sidebar(); ?>
-				</div>
+?>
+		</div>
+			<div class="col-xs-12 col-md-3 archive-sidebar">
+				<?php get_sidebar(); ?>
 			</div>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</div>
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_footer();
