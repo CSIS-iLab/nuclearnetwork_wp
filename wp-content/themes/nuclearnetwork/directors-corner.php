7d1f8f6b30df8e55289e64a841cfc7802c135ad4
Serven Maraghi <maraghiserven@gmail.com>
2020-02-04 00:03:38 -0500

    Add html structure for directors corner

--- wp-content/themes/nuclearnetwork/directors-corner.php
+++ wp-content/themes/nuclearnetwork/directors-corner.php
@@ -11,28 +11,42 @@
 
 get_header();
 
-if ( is_category() || is_tag() ) {
-	$img = get_option( 'nuclearnetwork_category_and_tag_archive_image' );
-} elseif ( is_author() ) {
-	$img = get_option( 'nuclearnetwork_authors_archive_image' );
-} else {
-	$img = get_archive_thumbnail_src( 'full' );
-}
+// if ( is_category() || is_tag() ) {
+// 	$img = get_option( 'nuclearnetwork_category_and_tag_archive_image' );
+// } elseif ( is_author() ) {
+// 	$img = get_option( 'nuclearnetwork_authors_archive_image' );
+// } else {
+// 	$img = get_archive_thumbnail_src( 'full' );
+// }
 
-$featured_img = ' style="background-image:url(\'' . esc_attr( $img ) . '\');"';
+// $featured_img = ' style="background-image:url(\'' . esc_attr( $img ) . '\');"';
 
 ?>
 
 <div id="primary" class="content-area">
 		<main id="main" class="site-main content-wrapper">
 			<header class="page-header"<?php echo $featured_img; ?>>
-				<div class="header-content"></div>
-			</header><!-- .page-header -->
+        <div class="header-content">
+          <h2><?php the_field('title'); ?></h2>
+          <h1><?php the_field('name'); ?></h1>
+        </div>
+        
+      </header><!-- .page-header -->
+      
 
 			<div class="content-wrapper row archive-container">
+				<div class="col-xs-12 col-md-9 archive-content director-content">
 
-				<div class="col-xs-12 col-md-9 archive-content">
-				
+      <div class="entry-content">
+        <!-- To friends of Poni Section -->
+      </div>
+            <div class="director-sidebar">
+              <img src="<?php the_field('image'); ?>" alt="director's photo"/>
+              <h4>to friends of poni</h4>
+              <p><?php the_field('community_message'); ?></p>
+                <!-- for looop here -->
+              <a class="btn btn-blue" href="">view all</a>   
+            </div>
 				
 				
 <?php
@@ -46,14 +60,16 @@ $featured_img = ' style="background-image:url(\'' . esc_attr( $img ) . '\');"';
 
 	the_posts_pagination();
 
-?>
-		</div>
-			<div class="col-xs-12 col-md-3 archive-sidebar">
-				<?php get_sidebar(); ?>
-			</div>
-		</div>
-	</main><!-- #main -->
-</div><!-- #primary -->
 
-<?php
-get_footer();
+	?>
+    </div>
+      <div class="col-xs-12 col-md-3 archive-sidebar">
+        <?php get_sidebar(); ?>
+      </div>
+    </div>
+
+    </main><!-- #main -->
+  </div><!-- #primary -->
+
+    <?php
+    get_footer();
