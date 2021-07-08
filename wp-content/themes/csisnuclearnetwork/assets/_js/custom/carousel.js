c312134b0295d37de442258a48c3d388cb4e8523
Lindsay Urchyk <urchykli@gmail.com>
2021-07-08 15:20:38 -0400

    Carousel options

--- wp-content/themes/csisnuclearnetwork/assets/_js/custom/carousel.js
+++ wp-content/themes/csisnuclearnetwork/assets/_js/custom/carousel.js
@@ -1,7 +1,12 @@
 import Splide from '@splidejs/splide'
-// new Splide('#splide').mount()
 
 const elms = document.getElementsByClassName('splide')
 for (let i = 0, len = elms.length; i < len; i++) {
-  new Splide(elms[i]).mount()
+  new Splide(elms[i], {
+    type: 'loop',
+    autoplay: true,
+    cover: true,
+    height: '8rem',
+    keyboard: 'focused',
+  }).mount()
 }
