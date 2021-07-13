d03766878d0d9ddf5d501c12cdaafc08c07837c8
Lindsay Urchyk <urchykli@gmail.com>
2021-07-13 12:29:26 -0400

    HTML scaffolding for Series archive headers

--- wp-content/themes/csisnuclearnetwork/template-parts/entry-header.php
+++ wp-content/themes/csisnuclearnetwork/template-parts/entry-header.php
@@ -24,7 +24,7 @@ $is_author = is_author(); //Author
 
 if ( $is_search || $is_tag || $is_category ) {
 	$image_URL = get_field( 'header_image', 'option' );
-} elseif ( $is_home ) {
+} elseif ( $is_home || $is_series) {
 	$image_URL = get_the_post_thumbnail_url( $page_for_posts );
 } else {
 	$image_URL = get_archive_thumbnail_src( 'nuclearnetwork-fullscreen' );
@@ -45,76 +45,99 @@ if ( $template === 'templates/template-no-image.php' ){
 
 <?php
 
-	if ( $is_archive ) { ?>
+	if ( $is_series ) { ?>
 
-			<header class="entry-header" <?php echo $feat_image; ?>>
+		<header class="entry-header" <?php echo $feat_image; ?>>
+
+			<?php 
+			the_archive_title( '<h1 class="entry-header__title"> Analysis / <span class="entry-header__title--secondary">', '</span></h1>' ); ?>
+
+			<div class="entry-header__desc"><?php echo term_description(); ?></div>
+
+			<a href="/analysis" class="entry-header__cta cta">All Analysis ></a>
+
+			<div class="entry-header__write-for-us">
+				<h2>Write for us placeholder</h2>
+				<p>Become a guest author with PONI to have your analysis published on our site. </p>
+			</div>
+			<?php
+
+			
+	} elseif ( $is_archive ) { ?>
+
+		<header class="entry-header" <?php echo $feat_image; ?>>
 				
 				<?php 
 			the_archive_title( '<h1 class="entry-header__title">', '</h1>' ); ?>
 			
 			<div class="entry-header__desc"><?php echo $description; ?></div>
 			<?php
-			
-		} elseif ( $is_home ) {
 
-			$description = get_field( 'archive_description', $page_for_posts );
+	} elseif ( $is_home ) {
+
+		$description = get_field( 'archive_description', $page_for_posts );
 			
-			$title = $object->post_title;
-			?>
-			<header class="entry-header" <?php echo $feat_image; ?>>
-				<?php
+		?>
+		<header class="entry-header" <?php echo $feat_image; ?>>
+			<?php
 			$post = get_page($page_for_posts);
 			setup_postdata($post); ?>
-				<h1 class="entry-header__title"><?php echo wp_kses_post( the_title() ); ?></h1>
-				<div class="entry-header__desc"><?php echo $description; ?></div>
-				<?php
+			<h1 class="entry-header__title"><?php echo wp_kses_post( the_title() ); ?></h1>
+			<div class="entry-header__desc"><?php echo $description; ?></div>
 
-		} elseif ( $is_search ) {
-			
-			$archive_title = sprintf(
-				'%1$s %2$s',
-				'<span class="entry-header__title-label">' . __( 'Search results for', 'nuclearnetwork' ) . '</span>',
-				'&lsquo;' . get_search_query() . '&rsquo;'
-			);
-			
-			?>
-			<header class="entry-header" <?php echo $feat_image; ?>>
-				
-				<h1 class="entry-header__title"><?php echo wp_kses_post( $archive_title ); ?></h1>
-				
-				<?php
-
-		} elseif ( $is_404 ) { ?>
-
-			<header class="entry-header" <?php echo $feat_image; ?>>
-				<h1 class="entry-header__title"><?php _e( '404', 'nuclearnetwork' ); ?></h1>
-
-			<?php 
-
-		} elseif ( $is_page ) { ?>
-			<header class="entry-header">
-
+			<div class="entry-header__write-for-us">
+				<h2>Write for us placeholder</h2>
+				<p>Become a guest author with PONI to have your analysis published on our site. </p>
+			</div>
 			<?php
+
+	} elseif ( $is_search ) {
+			
+		$archive_title = sprintf(
+			'%1$s %2$s',
+			'<span class="entry-header__title-label">' . __( 'Search results for', 'nuclearnetwork' ) . '</span>',
+			'&lsquo;' . get_search_query() . '&rsquo;'
+		);
+		
+		?>
+		<header class="entry-header" <?php echo $feat_image; ?>>
+				
+			<h1 class="entry-header__title"><?php echo wp_kses_post( $archive_title ); ?></h1>
+			
+			<?php
+
+	} elseif ( $is_404 ) { ?>
+
+		<header class="entry-header" <?php echo $feat_image; ?>>
+			<h1 class="entry-header__title"><?php _e( '404', 'nuclearnetwork' ); ?></h1>
+
+		<?php 
+
+	} elseif ( $is_page ) { ?>
+
+		<header class="entry-header">
+
+		<?php
 			the_title( '<h1 class="entry-header__title">', '</h1>' );
 
 			nuclearnetwork_page_desc();
 
-		} elseif ( is_single() ) { ?>
+	} elseif ( is_single() ) { ?>
 
-				<header class="entry-header">
+		<header class="entry-header">
 
-				<?php
-				echo '<div class="entry-header__header-content">';
-					the_title( '<h1 class="entry-header__title">', '</h1>' );
-					nuclearnetwork_page_desc();
-					nuclearnetwork_authors();
-					nuclearnetwork_posted_on();
-				echo '</div>';
-				if ( !$isNoImageTemplate ) {
-					get_template_part( 'template-parts/featured-image' );
-				}
+		<?php
+			echo '<div class="entry-header__header-content">';
+				the_title( '<h1 class="entry-header__title">', '</h1>' );
+				nuclearnetwork_page_desc();
+				nuclearnetwork_authors();
+				nuclearnetwork_posted_on();
+			echo '</div>';
+			if ( !$isNoImageTemplate ) {
+				get_template_part( 'template-parts/featured-image' );
+			}
 
-		}
+	}
 	?>
 
 </header><!-- .entry-header -->
