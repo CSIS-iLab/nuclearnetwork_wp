4637fec06c50191e2a402edc4a16a5f3574e5975
Lindsay Urchyk <urchykli@gmail.com>
2021-07-28 16:47:44 -0400

    Format event date in spans

--- wp-content/themes/csisnuclearnetwork/template-parts/block-post.php
+++ wp-content/themes/csisnuclearnetwork/template-parts/block-post.php
@@ -26,6 +26,14 @@ if ( $is_monthly_newsletter ) {
 	$classes .= ' post-block__featured';
 }
 
+if ( $event_info['event_start_date'] ) {
+	$event_day = wp_date('d', strtotime($event_info['event_start_date']));
+	$event_month = wp_date('M', strtotime($event_info['event_start_date']));
+	$event_year = wp_date('Y', strtotime($event_info['event_start_date']));
+
+	$event_date = '<span class="post-block__event-month">' . $event_month . '</span></br><span class="post-block__event-day">' . $event_day . '</span></br><span class="post-block__event-year">' . $event_year . '</span>';
+}
+
 if ( $event_info['event_start_time'] && $event_info['event_end_time'] ) {
 	$start_time = wp_date('g:i', strtotime($event_info['event_start_time']));
 	$end_time = wp_date('g:i A T', strtotime($event_info['event_end_time']));
@@ -53,7 +61,7 @@ if ( $event_info['event_start_time'] && $event_info['event_end_time'] ) {
 		nuclearnetwork_posted_on('Y');
 	} elseif ( $post_type === 'events' ) { ?>
 	<div class="post-block__event-content">
-		<div class="post-block__event-date"><?php echo $event_info['event_start_date']; ?></div>
+		<div class="post-block__event-date"><?php echo $event_date; ?></div>
 		<?php
 		nuclearnetwork_display_subtypes();
 		the_title( '<h3 class="post-block__title text--bold"><a href="' . esc_url( get_permalink() ) . '">', '</a></h3>' );
@@ -77,7 +85,6 @@ if ( $event_info['event_start_time'] && $event_info['event_end_time'] ) {
 		nuclearnetwork_posted_on('M j, Y');
 		the_excerpt();
 		nuclearnetwork_display_series();
-
 	} 
 	?>
 </article><!-- .post -->
