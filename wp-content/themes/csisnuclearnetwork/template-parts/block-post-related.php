870166e9ca939fb5caef3a6696d9f9ce04917f29
Kai Elwood-Dieu <kaielwooddieu@gmail.com>
2021-08-09 21:27:02 +0200

    basic first steps

--- wp-content/themes/csisnuclearnetwork/template-parts/block-post-related.php
+++ wp-content/themes/csisnuclearnetwork/template-parts/block-post-related.php
@@ -13,14 +13,10 @@
 
 ?>
 <article <?php post_class('post-block-related'); ?> id="post-<?php the_ID(); ?>">	
-		<?php if ( has_post_thumbnail() ) : ?>
-			<a href="<?php the_permalink(); ?>" class="post-block-related__img" title="<?php the_title_attribute(); ?>">
-				<?php the_post_thumbnail( 'large' ); ?>
-			</a>
-		<?php endif; ?>
-
 	<?php
-		nuclearnetwork_posted_on('M j, Y');
+		print_r("<h4 class='post-block-related__subtitle'>ANALYSIS</h4>");
+		nuclearnetwork_display_categories();
 		the_title( '<h3 class="post-block-related__title"><a class="post-block-related__title" href="' . esc_url( get_permalink() ) . '">', '</a></h3>' );
+		nuclearnetwork_posted_on('M j, Y');
 	?>
 </article><!-- .post-block-related -->
