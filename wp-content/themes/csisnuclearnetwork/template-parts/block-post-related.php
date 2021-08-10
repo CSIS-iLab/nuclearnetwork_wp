17cdeb805fc2c884968efef2d6fca490e9a1bf7d
Kai Elwood-Dieu <kaielwooddieu@gmail.com>
2021-08-10 17:33:05 +0200

    added basic scss

--- wp-content/themes/csisnuclearnetwork/template-parts/block-post-related.php
+++ wp-content/themes/csisnuclearnetwork/template-parts/block-post-related.php
@@ -13,10 +13,18 @@
 
 ?>
 <article <?php post_class('post-block-related'); ?> id="post-<?php the_ID(); ?>">	
+	<h4 class='post-block-related__subtitle'><a>ANALYSIS/</a><span><?php nuclearnetwork_display_categories() ?></span></h4>
 	<?php
-		print_r("<h4 class='post-block-related__subtitle'>ANALYSIS</h4>");
-		nuclearnetwork_display_categories();
 		the_title( '<h3 class="post-block-related__title"><a class="post-block-related__title" href="' . esc_url( get_permalink() ) . '">', '</a></h3>' );
 		nuclearnetwork_posted_on('M j, Y');
 	?>
+	<div class='post-block-related__byline'><span class="post-block-related__byline-divider">|</span>By. 
+		<span class="post-block-related__byline-authors">
+		<?php 
+			$authors = coauthors(); 
+			print_r($authors)
+		?>
+		</span>
+	</div>
+
 </article><!-- .post-block-related -->
