3370c7053098dba55ee9c82b2ccd14fe483032e2
Kai Elwood-Dieu <kaielwooddieu@gmail.com>
2021-08-10 18:12:43 +0200

    updating layout

--- wp-content/themes/csisnuclearnetwork/template-parts/block-post-related.php
+++ wp-content/themes/csisnuclearnetwork/template-parts/block-post-related.php
@@ -15,7 +15,7 @@
 <article <?php post_class('post-block-related'); ?> id="post-<?php the_ID(); ?>">	
 	<h4 class='post-block-related__subtitle'><a class="text-strong">ANALYSIS/</a><span><?php nuclearnetwork_display_categories() ?></span></h4>
 	<?php
-		the_title( '<h3 class="post-block-related__title"><a class="post-block-related__title" href="' . esc_url( get_permalink() ) . '">', '</a></h3>' );
+		the_title( '<h3 class="post-block-related__title text-stong"><a class="post-block-related__title" href="' . esc_url( get_permalink() ) . '">', '</a></h3>' );
 	?>
 	<div class='post-block-related__byline'>
 		<span class="post-block-related__byline-date"> 
