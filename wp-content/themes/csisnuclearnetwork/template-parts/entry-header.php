6e369b4cec81515c586d2c8ce4fd77d919152980
Kai Elwood-Dieu <kaielwooddieu@gmail.com>
2021-08-20 21:30:14 +0200

    removed changes to entry header file

--- wp-content/themes/csisnuclearnetwork/template-parts/entry-header.php
+++ wp-content/themes/csisnuclearnetwork/template-parts/entry-header.php
@@ -7,43 +7,13 @@
  * @since 1.0.0
  */
 
-$object = get_queried_object();
-
-$is_archive = is_archive(); //Not search, 404 or Analysis
+$is_archive = is_archive();
 $is_page = is_page();
 $has_thumbnail = has_post_thumbnail();
-$post_type = get_post_type(); //News, updates, projects, events, programs
-$is_404 = is_404(); //404
-$is_search = is_search(); //Search
-$is_home = is_home(); //Analysis
-$page_for_posts = get_option( 'page_for_posts' );
-$is_tag = is_tag(); //Tag
-$is_series = is_tax('series'); //Series
-$is_category = is_category(); //Category
-$is_author = is_author(); //Author
-$is_single = is_single();
-$post_parent_id = wp_get_post_parent_id(get_the_ID());
-
-if ( $is_search || $is_tag || $is_category ) {
-	$image_URL = get_field( 'header_image', 'option' );
-} elseif ( $is_home || $is_series) {
-	$image_URL = get_the_post_thumbnail_url( $page_for_posts );
-} else {
-	$image_URL = get_archive_thumbnail_src( 'nuclearnetwork-fullscreen' );
-}
-
-$feat_image = 'style="background-image:url('. $image_URL .');"';
-$headerClasses = 'entry-header--blue';
-if ( $is_author || $is_single && !$post_parent_id || is_singular( 'page' ) ) {
-  $headerClasses = 'entry-header--light';
-}
-$title_classes = 'entry-header__title entry-header__title--yellow';
-if ( $is_tag || $is_category || $is_search || $post_type === 'programs' && $is_single && $post_parent_id ) {
-	$title_classes = 'entry-header__title';
-}
-$description = get_field( 'archive_description', $object->name );
-$npn_link = get_field( 'nuclear_policy_news_link', 'option' );
-$monthly_news_link = get_field( 'monthly_newsletter_link', 'option' );
+$post_type = get_post_type();
+$is_404 = is_404();
+$is_search = is_search();
+$is_home = is_home();
 
 $template = get_page_template_slug( get_the_ID() );
 $isNoImageTemplate = false;
@@ -54,121 +24,47 @@ if ( $template === 'templates/template-no-image.php' ){
 
 ?>
 
-<header class="entry-header <?php echo $headerClasses; ?>">
+<header class="entry-header">
 
-<?php
+	<?php
 
-	if ( $is_series ) { ?>
+		if ( $is_archive ) {
 
-			<?php 
-			the_archive_title( '<h1 class="' . $title_classes . '"> Analysis / <span class="entry-header__title-secondary">', '</span></h1>' ); ?>
+			the_archive_title( '<h1 class="entry-header__title">', '</h1>' );
 
-			<div class="entry-header__grid">
+			the_archive_description('<div class="entry-header__desc">', '</div>');
 
-				<div class="entry-header__desc text--short"><?php echo term_description(); ?></div>
-				
-				<a href="/analysis" class="entry-header__cta cta btn btn--med">All Analysis <?php echo nuclearnetwork_get_svg( 'chevron-right' ); ?></a>
-				
-				<div class="entry-header__write-for-us desktop-only">
-					<?php dynamic_sidebar( 'write-for-us' ); ?>
-				</div>
-			</div>
-			<?php
+		} elseif ( $is_home ) {
+			$title = get_queried_object()->post_title;
+		?>
+			<h1 class="entry-header__title"><?php echo wp_kses_post( $title ); ?></h1>
+		<?php
+		} elseif ( $is_search ) {
 
-			
-	} elseif ( $is_search ) {
-			
-		$archive_title = sprintf(
+			$archive_title = sprintf(
 			'%1$s %2$s',
 			'<span class="entry-header__title-label">' . __( 'Search results for', 'nuclearnetwork' ) . '</span>',
 			'&lsquo;' . get_search_query() . '&rsquo;'
-		); ?>
-				
-			<h1 class="<?php echo $title_classes; ?>"><?php echo wp_kses_post( $archive_title ); ?></h1>
-			
-			<?php
+		);
 
-	} elseif ( $post_type === 'updates' && $is_archive ) {
-
-			the_archive_title( '<h1 class="' . $title_classes . '">', '</h1>' ); ?>
-
-		<div class="entry-header__grid">
-			<div class="entry-header__desc text--short"><?php echo $description; ?></div>
-			
-			<div class="entry-header__newsletter communication-block desktop-only">
-				<h2>Monthly Newsletter</h2>
-				<p>Get PONI Program Updates delivered directly to your inbox by signing up for our monthly newsletter!</p>
-				<a href="<?php echo $monthly_news_link; ?>" class="btn btn--teal subscribe">Subscribe</a>
-			</div>
-		</div>
-		<?php
-
-	} else if ( $post_type === 'programs' && $is_single && $post_parent_id ) {
-
-		$parent = $post->post_parent;
-
-		$parent_title = get_the_title( $parent);
-		$parent_url = get_permalink( $parent );
-
-		the_title( '<h1 class="' . $title_classes . '"><span class="entry-header__title-label">' . $parent_title . '</span>', '</h1>' );
 		?>
-		<a href="<?php echo $parent_url; ?>" class="entry-header__cta cta btn btn--med"><?php echo nuclearnetwork_get_svg( 'chevron-left' ); ?> Back to <?php echo $parent_title; ?></a>
+
+		<h1 class="entry-header__title"><?php echo wp_kses_post( $archive_title ); ?></h1>
 
 		<?php
 
-	} elseif ( $post_type === 'news' ) {
+		} elseif ( $is_404 ) { ?>
 
-		the_archive_title( '<h1 class="' . $title_classes . '">', '</h1>' ); ?>
-		<div class="entry-header__desc text--short"><?php echo $description; ?></div>
+			<h1 class="entry-header__title"><?php _e( '404', 'nuclearnetwork' ); ?></h1>
 
-		<a href="<?php echo $npn_link; ?>" class="btn entry-header__news-btn">Subscribe to the Newsletter</a>
-		<?php
-
-	} elseif ( $is_home ) {
-
-		$description = get_field( 'archive_description', $page_for_posts );
-		
-		$post = get_page($page_for_posts);
-		setup_postdata($post); ?>
-		<h1 class="<?php echo $title_classes; ?>"><?php echo wp_kses_post( the_title() ); ?></h1>
-
-		<div class="entry-header__grid">
-			<div class="entry-header__desc text--short"><?php echo $description; ?></div>
-
-			<div class="entry-header__write-for-us desktop-only">
-			<?php dynamic_sidebar( 'write-for-us' ); ?>
-			</div>
-		</div>
-		<?php
-
-	} elseif ( $is_category || $is_tag ) {
-
-		the_archive_title( '<h1 class="' . $title_classes . '">', '</h1>' ); ?>
-		
-		<div class="entry-header__desc text--short"><?php echo $description; ?></div>
-		<?php
-
-	} elseif ( $is_404 ) { ?>
-
-			<h1 class="<?php echo $title_classes; ?>"><?php _e( '404', 'nuclearnetwork' ); ?></h1>
-
-		<?php 
-
-	} elseif ( $is_archive ) {
-
-		the_archive_title( '<h1 class="' . $title_classes . '">', '</h1>' ); ?>
-		
-		<div class="entry-header__desc text--short"><?php echo $description; ?></div>
-		<?php
-
-	} elseif ( $is_page ) { 
+		<?php } elseif ( $is_page ) {
 			the_title( '<h1 class="entry-header__title">', '</h1>' );
 
 			nuclearnetwork_page_desc();
+		}
 
-	} elseif ( $is_single ) { 
+		elseif ( is_single() ) {
 			echo '<div class="entry-header__header-content">';
-				nuclearnetwork_display_subtypes();
 				the_title( '<h1 class="entry-header__title">', '</h1>' );
 				nuclearnetwork_page_desc();
 				nuclearnetwork_authors();
@@ -178,7 +74,7 @@ if ( $template === 'templates/template-no-image.php' ){
 				get_template_part( 'template-parts/featured-image' );
 			}
 
-	}
+		}
 	?>
 
 </header><!-- .entry-header -->
