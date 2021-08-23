81fca66e304670e494726b889f27eedb5606f431
Kai Elwood-Dieu <kaielwooddieu@gmail.com>
2021-08-23 18:26:26 +0200

    updated links and additional comments

--- wp-content/themes/csisnuclearnetwork/template-parts/newsletter-block-acf.php
+++ wp-content/themes/csisnuclearnetwork/template-parts/newsletter-block-acf.php
@@ -22,11 +22,29 @@
     'post_type'   => 'news',
     'fields' => 'ids'
     );
+
+    $progUpdate_args = array(
+    'numberposts' => 1,
+    'post_type'   => 'updates',
+    'meta-key' => 'newsletter',
+    'meta-value' => 'true'
+    );
     
     $latest_news = get_posts( $news_args );
 
+    $latest_updates = get_posts( $progUpdate_args );
+
     if( $latest_news ) {
         $recentNewsUrl = get_permalink($latest_news[0]);
+    }
+
+    if ($latest_updates) {
+        print_r("This is working");
+        print_r($latest_updates);
+        $recentUpdatesUrl = get_permalink($latest_updates[0]);
+    } else {
+        print_r("shit" . $latest_updates);
+        echo $recentUpdatesUrl;
     }
 
     $dailyNewsletterName = get_field('daily_newsletter_name');
@@ -34,13 +52,13 @@
     $dailyNewsletterLinks = "<div class='recent-news'><a href=" . $recentNewsUrl . ">Most recent newsletter" .nuclearnetwork_get_svg('chevron-right') . "</a><a href=" . get_post_type_archive_link( 'news' ) . ">All Nuclear Policy News" .nuclearnetwork_get_svg('chevron-right') . "</a></div>";
     $monthlyNewsletterName = get_field('monthly_newsletter_name');
     $monthlyNewsletterDesc = get_field('monthly_newsletter_description');
-    $monthlyNewsletterLinks = "<div class='recent-news'><a href=" . $recentNewsUrl . ">Most recent newsletter" .nuclearnetwork_get_svg('chevron-right') . "</a></div>";
+    $monthlyNewsletterLinks = "<div class='recent-news'><a href=" . $recentUpdatesUrl . ">Most recent newsletter" .nuclearnetwork_get_svg('chevron-right') . "</a></div>";
 
-    $constructedDailyNewsletter = "<div class='daily'><h3>" . $dailyNewsletterName . "</h3><p>" . $dailyNewsletterDesc . "</p><a class='subscribe btn btn--teal btn--newsletter' href=" . get_field('nuclear_policy_news_link', 'option') . ">Subscribe</a>" . $dailyNewsletterLinks . "</div>";
+    $constructedDailyNewsletter = "<div class='daily'><h3>" . $dailyNewsletterName . "</h3><p class=''text--short>" . $dailyNewsletterDesc . "</p><a class='subscribe btn btn--teal btn--newsletter' href=" . get_field('nuclear_policy_news_link', 'option') . ">Subscribe</a>" . $dailyNewsletterLinks . "</div>";
 
-    $constructedMonthlyNewsletter = "<div class='monthly'><h3>" . $monthlyNewsletterName . "</h3><p>" . $monthlyNewsletterDesc . "</p><a class='subscribe btn btn--teal btn--newsletter' href=" . get_field('monthly_newsletter_link', 'option') . ">Subscribe</a>" . $monthlyNewsletterLinks . "</div>";
+    $constructedMonthlyNewsletter = "<div class='monthly'><h3>" . $monthlyNewsletterName . "</h3><p class=''text--short>" . $monthlyNewsletterDesc . "</p><a class='subscribe btn btn--teal btn--newsletter' href=" . get_field('monthly_newsletter_link', 'option') . ">Subscribe</a>" . $monthlyNewsletterLinks . "</div>";
 
-    $output = "<div class='newsletter-block-acf'><h3 class='home__subtitle'>Newsletters</h3>" . $constructedDailyNewsletter . $constructedMonthlyNewsletter . "</div>";
+    $output = "<div class='newsletter-block-acf'><h2 class='home__subtitle'>Newsletters</h3>" . $constructedDailyNewsletter . $constructedMonthlyNewsletter . "</div>";
     
     echo $output;
 ?>
