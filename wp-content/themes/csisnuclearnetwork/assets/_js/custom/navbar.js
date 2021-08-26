0e0564641486997f8175c6ad62d4255878a0774a
Lindsay Urchyk <urchykli@gmail.com>
2021-08-26 08:41:02 -0400

    Add class and aria lable to parent menu when clicked

--- wp-content/themes/csisnuclearnetwork/assets/_js/custom/navbar.js
+++ wp-content/themes/csisnuclearnetwork/assets/_js/custom/navbar.js
@@ -7,10 +7,12 @@ const trigger = document.querySelector('.site-nav__trigger')
 const menu = document.querySelector('.site-nav__container')
 const nav = document.querySelector('.site-nav__menu')
 const parentMenus = document.querySelectorAll('.sub-menu')
+const parentEls = document.querySelectorAll('.menu-item-has-children')
 
 const Navigation = () => {
   menuFocus()
   toggleMenu()
+  toggleSubmenu()
   closeMenu()
 }
 
@@ -52,9 +54,24 @@ const toggleMenu = () => {
   })
 }
 
+const toggleSubmenu = () => {
+  parentEls.forEach((parentEl) => {
+    parentEl.addEventListener('click', function () {
+      if (parentEl.classList.contains('is-active')) {
+        this.setAttribute('aria-expanded', 'false')
+        this.classList.remove('is-active')
+      } else {
+        this.setAttribute('aria-expanded', 'true')
+        this.classList.add('is-active')
+      }
+    })
+  })
+}
+
 const closeMenu = () => {
   document.addEventListener('click', function (e) {
     const buttonClicked = e.target === trigger || e.target.parentElement === trigger
+    console.log(e)
 
     if (!nav.contains(e.target) && !buttonClicked) {
       trigger.setAttribute('aria-expanded', 'false')
