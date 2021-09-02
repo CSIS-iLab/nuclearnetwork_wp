3e1c9cd4996d9882a3cc3a1dbdde7238f306ba30
Lindsay Urchyk <urchykli@gmail.com>
2021-09-02 15:11:30 -0400

    Related post section code added to jetpack related posts shortcode

--- wp-content/themes/csisnuclearnetwork/inc/shortcodes.php
+++ wp-content/themes/csisnuclearnetwork/inc/shortcodes.php
@@ -59,36 +59,48 @@ add_shortcode( 'share', 'nuclearnetwork_shortcode_share_button' );
 function jetpackme_custom_related( $atts ) {
 
 	if ( class_exists( 'Jetpack_RelatedPosts' ) && method_exists( 'Jetpack_RelatedPosts', 'init_raw' ) ) {
-			$related = Jetpack_RelatedPosts::init_raw()
+			$related_posts = Jetpack_RelatedPosts::init_raw()
 					->set_query_name( 'jetpackme-shortcode' ) // Optional, name can be anything
 					->get_for_post_id(
 							get_the_ID(),
 							array( 'size' => 4 )
 					);
 
-		if ( $related || has_tag() ) {
-			echo '<h2 class="single__footer-heading">';
-			echo nuclearnetwork_get_svg( "3-arrows" );
-			_e( 'Related Content', 'nuclearnetwork' );
-			echo '</h2>';
-			nuclearnetwork_display_tags();
+	// if no related content we bail
+	if ( !($related_posts || has_tag() || has_category()) ) {
+		return;
+	}
 
-			if ( $related ) {
-				wp_reset_postdata();
-				echo '<ul class="related-posts" role="list">';
-				foreach ( $related as $result) {
-					global $post;
-					$post = get_post($result['id']);
-					
-					setup_postdata($post);
-					
-					echo '<li>';
-					get_template_part( 'template-parts/block-post-related', get_post_format() );
-					echo '</li>';
-				}
-				echo '</ul>';
-			}
-		}
+	echo '<hr />';
+	echo '<h2 class="single__footer-heading">';
+			_e( 'Related', 'nuclearnetwork' );
+	echo '</h2>';
+	echo '<div class="single__footer-related-posts">';
+
+	if ( $related_posts ) {
+			echo '<ul class="related-posts" role="list">';
+					foreach( $related_posts as $post ):
+							setup_postdata($post);
+							echo '<li>';
+							get_template_part( 'template-parts/block-post-related' );
+							echo '</li>';
+					endforeach;
+			echo '</ul>';
+			wp_reset_postdata();
+	}
+	echo '</div>';
+
+	echo '<div class="single__footer-related-terms">';
+	if (has_category()) {
+		echo '<h3 class="single__footer-label">Topics</h3>';
+		nuclearnetwork_display_categories();
+	}
+
+	if ( has_tag()) {
+		echo '<h3 class="single__footer-label">Keywords</h3>';
+		nuclearnetwork_display_tags();
+	}
+	echo '</div>';
 	}
 
 }
