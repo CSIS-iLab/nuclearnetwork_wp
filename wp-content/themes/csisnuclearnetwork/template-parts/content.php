6103e46ff4864368e88c8974650087f7d1921b4e
Lindsay Urchyk <urchykli@gmail.com>
2021-09-03 12:51:42 -0400

    Author section grid on large breakpoint

--- wp-content/themes/csisnuclearnetwork/template-parts/content.php
+++ wp-content/themes/csisnuclearnetwork/template-parts/content.php
@@ -33,12 +33,13 @@
 		<?php
 
 		    echo '<div class="post__authors"><hr>';
-			echo '<h2 class="section__heading post__authors-heading">Authors</h2><p class="text--italic text--short post__authors-disclaimer">The views expressed above are the author’s and do not necessarily reflect those of the Center for Strategic and International Studies or the Project on Nuclear Issues.</p><div class="post__authors-group">';
+			echo '<h2 class="section__heading post__authors-heading">Authors</h2><p class="text--italic text--short post__authors-disclaimer">The views expressed above are the author’s and do not necessarily reflect those of the Center for Strategic and International Studies or the Project on Nuclear Issues.</p><div class="post__authors-content"><div class="post__authors-group">';
 			nuclearnetwork_authors_list_extended(); 
-			echo '</div><div class="single__footer__write-for-us">';
+			echo '</div><div class="single__footer-write-for-us">';
 			dynamic_sidebar('write-for-us'); 
-			echo '</div></div>';
+			echo '</div>';
 			nuclearnetwork_citation();
+			echo '</div></div>';
 			// echo do_shortcode( '[jprel]' );
 			// if ( function_exists( 'ADDTOANY_SHARE_SAVE_KIT' ) ) { ADDTOANY_SHARE_SAVE_KIT(); }
 			// get_template_part( 'template-parts/featured-image-caption' );
