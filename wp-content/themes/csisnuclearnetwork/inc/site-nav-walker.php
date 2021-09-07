4096352d3d09a8d10143b437a9dbf7109962a00e
Lindsay Urchyk <urchykli@gmail.com>
2021-09-07 15:39:34 -0400

    Remove bootstrap classes from nav walker

--- wp-content/themes/csisnuclearnetwork/inc/site-nav-walker.php
+++ wp-content/themes/csisnuclearnetwork/inc/site-nav-walker.php
@@ -45,10 +45,10 @@ class NuclearNetwork_Menu extends Walker_Nav_Menu {
 
         $indent = str_repeat("\t", $depth);
         $output .= "\n$indent<div class='sub-menu-container sub-menu-container-depth-" . $depth . " row'>
-            <div class='col-xs-12 col-md sub-menu-children' aria-expanded='false'>\n";
+            <div class='sub-menu-children' aria-expanded='false'>\n";
 
         if ( $args->has_featured ) {
-          $output .= "<div class='hidden-xs col-md-4 sub-menu-featured-post'>
+          $output .= "<div class='hidden-xs sub-menu-featured-post'>
                       " . $featured_post_html . "
                       </div>\n";
         }
