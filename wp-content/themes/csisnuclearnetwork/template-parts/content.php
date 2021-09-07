24d80ae4832cc270b3aff07dc9ae50334f8b8737
Lindsay Urchyk <urchykli@gmail.com>
2021-09-07 14:31:52 -0400

    Move share buttons to correct location

--- wp-content/themes/csisnuclearnetwork/template-parts/content.php
+++ wp-content/themes/csisnuclearnetwork/template-parts/content.php
@@ -25,6 +25,7 @@
 		<?php
       		the_content( __( 'Continue reading', 'nuclearnetwork' ) );
 			nuclearnetwork_display_footnotes();
+			if ( function_exists( 'ADDTOANY_SHARE_SAVE_KIT' ) ) { ADDTOANY_SHARE_SAVE_KIT(); }
 		?>
 	</div><!-- .post-inner -->
 
@@ -41,11 +42,9 @@
 			nuclearnetwork_citation();
 			echo '</div></div>';
 			echo do_shortcode( '[jprel]' );
-			if ( function_exists( 'ADDTOANY_SHARE_SAVE_KIT' ) ) { ADDTOANY_SHARE_SAVE_KIT(); }
 			// get_template_part( 'template-parts/featured-image-caption' );
 			get_template_part( 'template-parts/post-related-content' );
 		?>
-		// ?>
 	</footer>
 	<?php } ?>
 
