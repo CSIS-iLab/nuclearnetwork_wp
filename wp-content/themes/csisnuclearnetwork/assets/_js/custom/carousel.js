4ac3bc56fe8a95b7e9f7281722e04bf3e4fc1e92
Jacque Schrag <jnschrag@gmail.com>
2021-09-07 21:53:26 -0400

    Style pagination & arrows

--- wp-content/themes/csisnuclearnetwork/assets/_js/custom/carousel.js
+++ wp-content/themes/csisnuclearnetwork/assets/_js/custom/carousel.js
@@ -2,11 +2,12 @@ import Splide from '@splidejs/splide'
 
 const elms = document.getElementsByClassName('js-splide')
 for (let i = 0, len = elms.length; i < len; i++) {
-  new Splide(elms[i], {
-    // type: 'loop',
-    // autoplay: true,
-    // cover: true,
-    // height: '8rem',
+  const splide = new Splide(elms[i], {
+    type: 'loop',
+    autoplay: true,
     keyboard: 'focused',
   }).mount()
+
+  // Set the number of items to use in CSS calculations
+  splide.root.style.setProperty('--num-items', splide.length)
 }
