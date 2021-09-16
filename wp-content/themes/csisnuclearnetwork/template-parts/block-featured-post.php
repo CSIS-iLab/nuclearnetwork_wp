dc91d6d917c4da96d05ca736dd01733adea1818e
Lindsay Urchyk <urchykli@gmail.com>
2021-09-16 17:55:28 -0400

    Adjust aspect ration to account for gradient divs

--- wp-content/themes/csisnuclearnetwork/template-parts/block-featured-post.php
+++ wp-content/themes/csisnuclearnetwork/template-parts/block-featured-post.php
@@ -47,7 +47,7 @@ if ( $legacy_event_date ) {
 ?>
 <article <?php post_class( $classes ); ?> id="post-<?php the_ID(); ?>">	
 		<?php if ( $args[0] == "true" ) : ?>
-				<?php echo "<div class='feat-post__image' style='background-image: linear-gradient(180deg, rgba(0, 0, 0, 0) 66.55%, rgba(10, 35, 58, 0.7) 100%), url(" . get_the_post_thumbnail_url() . ") ;'><div class='feat-post__gradient-top'></div><div class='feat-post__gradient-bottom'></div></div>" ?>
+				<?php echo "<div class='aspect-ratio'><div class='feat-post__image' style='background-image: linear-gradient(180deg, rgba(0, 0, 0, 0) 66.55%, rgba(10, 35, 58, 0.7) 100%), url(" . get_the_post_thumbnail_url() . ") ;'><div class='feat-post__gradient-top'></div><div class='feat-post__gradient-bottom'></div></div></div>" ?>
 			<!-- <div class="feat-post__gradient-top"></div>
 			<div class="feat-post__gradient-bottom"></div> -->
 			<?php endif; ?>
