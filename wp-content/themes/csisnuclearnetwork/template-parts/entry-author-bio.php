c43e43707ddbbb87c1833c36900d6a76fd853e80
Lindsay Urchyk <urchykli@gmail.com>
2021-09-17 22:07:02 -0400

    Author information is displaying

--- wp-content/themes/csisnuclearnetwork/template-parts/entry-author-bio.php
+++ wp-content/themes/csisnuclearnetwork/template-parts/entry-author-bio.php
@@ -7,34 +7,33 @@
  * @since 1.0.0
  */
 
+global $coauthors_plus;
+
 $author = get_queried_object();
 $author_id = $author->ID;
 
-foreach ( get_coauthors() as $coauthor ) {
-	var_dump($coauthor);
+
+$coauthor_data = $coauthors_plus->get_coauthor_by( 'id', $author_id );
+$title = get_field( 'title', $author_id );
+$bio_file = get_field( 'bio_file', $author_id );
+$is_senior = get_field( 'senior_staff', $author_id );
+$user_bio = $coauthor_data->description;
+if ( !$user_bio ) {
+	$user_bio = get_user_meta( $author_id, 'description', true );
+} elseif ( !$user_bio) {
+	$user_bio = get_field( 'short_bio', $author_id );
 }
-nuclearnetwork_authors_list_extended(); 
-if ( (bool) get_the_author_meta( 'description' ) && (bool) get_theme_mod( 'show_author_bio', true ) ) : ?>
+
+?>
 <div class="author-bio">
-	<div class="author-title-wrapper">
 		<div class="author-avatar vcard">
 			<?php echo get_avatar( get_the_author_meta( 'ID' ), 160 ); ?>
 		</div>
-		<h2 class="author-title heading-size-4">
-			<?php
-			printf(
-				/* translators: %s: Author name */
-				__( 'By %s', 'nuclearnetwork' ),
-				esc_html( get_the_author() )
-			);
-			?>
-		</h2>
-	</div><!-- .author-name -->
 	<div class="author-description">
-		<?php echo wp_kses_post( wpautop( get_the_author_meta( 'description' ) ) ); ?>
-		<a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-			<?php _e( 'View Archive <span aria-hidden="true">&rarr;</span>', 'nuclearnetwork' ); ?>
-		</a>
+		<?php echo wp_kses_post( wpautop( $user_bio ) ); ?>
 	</div><!-- .author-description -->
+		<?php if ( $is_senior === true ) {
+			echo '<a href="' . $bio_file['url'] . '" class="btn btn--large btn--outline-dark">Download Bio and Headshot ' . nuclearnetwork_get_svg( 'download' ) .'</a>';
+		}
+		?>
 </div><!-- .author-bio -->
-<?php endif; ?>
